0476b5fcfc73ac7ab9e9fcf6e9d3928b10dc2d8b
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-03-29 13:19:17 -0300

    Criado projeto; Criado componente Header

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from './Header'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          
+        </main>
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container-fluid bg-warning text-light">
+                <div className="row">
+                    <div className="col-6 offset-3 text-center">
+                        <h1>Todux app</h1>
+                        <h2>Crie uma lista de afazeres rapidamente</h2>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
